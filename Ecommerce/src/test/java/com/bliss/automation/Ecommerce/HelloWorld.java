6070db7a2b95b6b85516e2df91fd52913a212715
blisstestingtools <blisstestingtools@gmail.com>
2023-01-07 20:11:59 +0530

    First commit

---
+++ Ecommerce/src/test/java/com/bliss/automation/Ecommerce/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.bliss.automation.Ecommerce;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\home\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+		WebDriver drver=new ChromeDriver();
+	}
+
+}
